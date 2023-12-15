97106e8add93399651877e948b3a6c112a5bf1b2
rhoeunbin <rhoeb21@gmail.com>
2023-12-15 17:42:02 +0900

    feat : realtime tweets => 실시간 쿼리 변경

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,10 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
+import { limit } from "firebase/firestore/lite";
 
 export interface ITweet {
   id: string;
@@ -13,31 +15,58 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc")
+        //limit(25) // 글 개수 제한 가능
+      );
+      // const snapshot = await getDocs(tweetsQuery);
+      // const tweets = snapshot.docs.map((doc) => {
+      //   const { tweet, createdAt, userId, username, photo } = doc.data();
+      //   return {
+      //     tweet,
+      //     createdAt,
+      //     userId,
+      //     username,
+      //     photo,
+      //     id: doc.id,
+      //   };
+      // });
+
+      // 문서를 한 번만 가져오는 대신 쿼리에 리스너 추가
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        // user가 보고 있지 않으면 리스너를 사용할 필요가 없음
+        const tweets = snapshot.docs.map((doc) => {
+          // map을 사용하여 배열로 저장
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
+
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,7 +5,7 @@ import Timeline from "../components/timeline";
 const Wrapper = styled.div`
   display: grid;
   gap: 50px;
-  overflow-y: scroll; // 트윗들을 스크롤
+  // overflow-y: scroll; // 트윗들을 스크롤
   grid-template-rows: 1fr 5fr;
 `;
 
